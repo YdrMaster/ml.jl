22cd1b94078852c4d4c8b912a3567f3d4d4e2a22
YdrMaster <ydrml@hotmail.com>
2023-01-06 20:03:59 +0800

    update
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- nn-old/nn.jl
+++ nn-old/nn.jl
@@ -9,20 +9,22 @@ Network{T} = Vector{Matrix{T}}
 用随机初始权构造神经网络
 每行是一个神经元
 """
-Network(sizes::Vector{Int}) = [Random.randn(sizes[i], sizes[i - 1] + 1) for i in 2:length(sizes)]
+Network(sizes::Vector{Int}) = [Random.randn(sizes[i], sizes[i-1] + 1) for i in 2:length(sizes)]
 
 "神经网络的规模"
-structof(network::Network) =            # 神经网络的规模等于
-    [[size(w, 2) - 1 for w in network]; # 权重矩阵每项的列数
-     size(network[end], 1)]             # 最后一项的行数
+structof(network::Network) =
+    [
+        [size(w, 2) - 1 for w in network] # 权重矩阵每项的列数
+        size(network[end], 1)             # 最后一项的行数
+    ]
 
 "获得 `l-1` 层的第 `k` 个神经元到 `l` 层第 `j` 个神经元的连接强度"
 w(network::Network, j::Integer, k::Integer, l::Integer) =
-    network[l - 1][j,k]
+    network[l-1][j, k]
 
 "获得 `l` 层的第 `j` 个神经元的偏置"
 b(network::Network, j::Integer, l::Integer) =
-    network[l - 1][j,end]
+    network[l-1][j, end]
 
 sigmoid(z::Real) = 1 / (1 + exp(-z))
 dsigmoid(z::Real) = (exp_z = exp(-z); exp_z / (1 + exp_z)^2)
@@ -80,22 +82,22 @@ function sgd!(
     for _ in 1:epochs
         indices = Random.shuffle(1:n)
         # 训练集分为 `length(training_data) ÷ mini_batch_size` 批
-        @time for i in 1:(n ÷ mini_batch_size)
-            @views _indices = indices[(1:mini_batch_size) .+ (i - 1)mini_batch_size]
+        @time for i in 1:(n÷mini_batch_size)
+            @views _indices = indices[(1:mini_batch_size).+(i-1)mini_batch_size]
             # 用每组数据执行反向传播
             for index in _indices
-                @views as[1][1:end - 1] .= training_data[1:network_struct[1],index]
+                @views as[1][1:end-1] .= training_data[1:network_struct[1], index]
                 # 前向传播
                 for (i, w) in enumerate(network)
                     mat_mul!(zs[i], w, as[i])
-                    map!(sigmoid, as[i + 1], zs[i])
+                    map!(sigmoid, as[i+1], zs[i])
                     map!(dsigmoid, δs[i], zs[i])
                 end
                 # 反向传播
-                @views δs[end] .*= as[end][1:end - 1] .- training_data[network_struct[1] + 1:end,index]
+                @views δs[end] .*= as[end][1:end-1] .- training_data[network_struct[1]+1:end, index]
                 for i in length(network):-1:2
-                    @views mat_mul!(buffer_mat_mul[i - 1], transpose(network[i][:,1:end - 1]), δs[i])
-                    δs[i - 1] .*= buffer_mat_mul[i - 1]
+                    @views mat_mul!(buffer_mat_mul[i-1], transpose(network[i][:, 1:end-1]), δs[i])
+                    δs[i-1] .*= buffer_mat_mul[i-1]
                 end
                 for i in eachindex(∇w)
                     mat_mul!(∇w[i], δs[i], transpose(as[i]), -η, 1)
@@ -114,7 +116,7 @@ function sgd!(
                 # a = [round(UInt8, x) for x in feedforward(network, test_data[1:network_struct[1],i])]
                 # a == test_data[network_struct[1] + 1:end,i]
                 # 概率分布
-                argmax(feedforward(network, test_data[1:network_struct[1],i])) == argmax(test_data[network_struct[1] + 1:end,i])
+                argmax(feedforward(network, test_data[1:network_struct[1], i])) == argmax(test_data[network_struct[1]+1:end, i])
             end
             push!(errors, m / n)
             # GR.plot(errors)
---
+++ nndl/ch1/nn.jl
@@ -0,0 +1,80 @@
+﻿import Random, .Iterators.partition
+
+"神经网络"
+struct Network
+    weights::Vector{Matrix{Float64}}
+    Network(sizes::Integer...) = new([Random.randn(sizes[i], sizes[i-1] + 1) for i in eachindex(sizes)[2:end]])
+end
+
+"sigmoid function"
+σ(z) = 1 / (1 + exp(-z))
+
+"diff sigmoid function"
+dσ(z) = (x -> x * (1 - x))(σ(z));
+
+"前馈"
+feedforward(network::Network, input::Vector) = foldl((acc, w) -> σ.(w * [acc; 1]), network.weights; init=input)
+
+"数据集"
+DataSet = AbstractVector{Tuple{AbstractVector{<:Real},Real}}
+
+"随机梯度下降"
+function sgd(
+    network::Network,
+    training_data::DataSet,
+    η::Real,
+    epochs::Integer,
+    mini_batch_size::Integer,
+    test_data::Union{DataSet,Nothing}=nothing,
+)
+    for epoch in 1:epochs
+        Random.shuffle!(training_data)
+        for mini_batch in partition(training_data, mini_batch_size)
+            _update_mini_batch!(network, mini_batch, η)
+        end
+        if test_data !== nothing
+            @show evaluate(network, test_data)
+        else
+            @show epoch
+        end
+    end
+end
+
+"小批量更新"
+function _update_mini_batch!(
+    network::Network,
+    mini_batch::DataSet,
+    η::Real,
+)
+    η /= length(mini_batch)
+    ∇w = [zeros(size(w)) for w in network.weights]
+    for (x, y) in mini_batch
+        ∇w .+= backprop(network, x, y)
+    end
+    network.weights .-= η .* ∇w
+end
+
+"反向传播"
+function backprop(
+    network::Network,
+    x::AbstractVector{<:Real},
+    y::Real,
+)
+    ∇w = [zeros(size(w)) for w in network.weights]
+
+    as = [x] # activations 保存每层所有激活值
+    zs = []  # 保存每层的所有 z
+    for w in network.weights
+        z = w * [as[end]; 1]
+        push!(zs, z)
+        a = σ.(z)
+        push!(as, a)
+    end
+    δ = (as[end] - y) .* dσ.(zs[end])
+    ∇w[end] = δ * [as[end-1]; 1]'
+    for l in 2:length(network.weights)
+        δ = (network.weights[end-l+1]'*δ)[1:end-1] .* dσ.(zs[end-l])
+        ∇w[end-l+1] = δ * [as[end-l]; 1]
+    end
+    ∇w
+end
